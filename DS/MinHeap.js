b31f9d0890ac7a73d0e0a62eda9c815200d99e62
wandergithub <wandergonzalez0@hotmail.com>
2023-03-10 01:05:08 -0400

    Add DS MinHeap class

---
+++ DS/MinHeap.js
@@ -0,0 +1,16 @@
+/*
+    MinHeap implementation
+*/
+
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  getMin() {
+    // Get the min element at index 0 in the heap array
+    this.heap.shift();
+  }
+
+  
+}
